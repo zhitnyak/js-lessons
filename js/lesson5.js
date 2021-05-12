019c7947cd8a49cd46ea506dc93ec2cf0f177c4f
Zhitnyak Maksim <m.zhitnyak@gmail.com>
2021-05-12 23:04:39 +0300

    5-16 task

--- js/lesson5.js
+++ js/lesson5.js
@@ -1,15 +1,167 @@
-const parent = {
-  name: "Stacey",
-  surname: "Moore",
-  age: 54,
-  heritage: "Irish",
-};
-// Пиши код ниже этой строки
+// const parent = {
+//   name: "Stacey",
+//   surname: "Moore",
+//   age: 54,
+//   heritage: "Irish",
+// };
+// // Пиши код ниже этой строки
 
-const child = Object.create(parent);
+// const child = Object.create(parent);
 
-// Пиши код выше этой строки
-child.name = "Jason";
-child.age = 27;
-consoole.log(parent.hasOwnProperty("surname"));
-console.log(parent.hasOwnProperty("heritage"));
+// // Пиши код выше этой строки
+// child.name = "Jason";
+// child.age = 27;
+// consoole.log(parent.hasOwnProperty("surname"));
+// console.log(parent.hasOwnProperty("heritage"));
+
+// //---------5-3------
+// function Car(brand, model, price) {
+//   this.model = model;
+//   this.brand = brand;
+//   this.price = price;
+// }
+
+//---------5-5---------
+// function Car({ brand, model, price }) {
+//   this.model = model;
+//   this.brand = brand;
+//   this.price = price;
+// }
+
+// Car.prototype.getPrice = function () {
+//   return this.price;
+// };
+
+// Car.prototype.changePrice = function (newPrice) {
+//   let message = `У автомобиля ${this.brand} ${this.model} цена ${this.price} заменена на ${newPrice}`;
+//   this.price = newPrice;
+//   return message;
+// };
+
+// const newCar = new Car({ brand: "Audi", model: "Q3", price: 6000 });
+// console.log(newCar.getPrice());
+// console.log(newCar.changePrice(5000));
+// console.log(newCar.getPrice());
+
+//---------5-6------
+// function Storage(items) {
+//   this.items = items;
+// }
+// Storage.prototype.getItems = function () {
+//   return this.items;
+// };
+// Storage.prototype.addItem = function (newItem) {
+//   this.items.push(newItem);
+//   console.log(newItem);
+// };
+// Storage.prototype.removeItem = function (item) {
+//   this.items.splice(this.items.indexOf(item), 1);
+//   console.log(item);
+// };
+// // Storage.prototype.removeItem(item);
+// // return item;
+// // Пиши код выше этой строки
+// const storage = new Storage(["Нанитоиды", "Пролонгер", "Антигравитатор"]);
+// console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор"]
+// storage.addItem("Дроид");
+// console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор", "Дроид"]
+// storage.removeItem("Пролонгер");
+
+//-------4-7-----
+// function StringBuilder(baseValue) {
+//   this.value = baseValue;
+// }
+// StringBuilder.prototype.getValue = function () {
+//   return this.value;
+// };
+// StringBuilder.prototype.padEnd = function (str) {
+//   this.value = this.value + str;
+// };
+// StringBuilder.prototype.padStart = function (str) {
+//   this.value = str + this.value;
+// };
+// StringBuilder.prototype.padBoth = function (str) {
+//   this.value = str + this.value + str;
+// };
+// // Пиши код выше этой строки
+// const builder = new StringBuilder(".");
+// console.log(builder.getValue()); // '.'
+// builder.padStart("^");
+// console.log(builder.getValue()); // '^.'
+// builder.padEnd("^");
+// console.log(builder.getValue()); // '^.^'
+// builder.padBoth("=");
+// console.log(builder.getValue()); // '=^.^='
+
+//--------5-9------
+// class Car {
+//   brand;
+//   model;
+//   price;
+//   constructor({ brand, model, price }) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+// }
+// console.log(new Car({ brand: "BMW", model: "X5", price: 58900 }));
+
+//----------5-12------
+// class Storage {
+//   #items;
+//   constructor(items) {
+//     this.#items = items;
+//   }
+
+//   getItems() {
+//     return this.#items;
+//   }
+
+//   addItem(newItem) {
+//     this.#items.push(newItem);
+//   }
+
+//   removeItem(item) {
+//     const itemIndex = this.#items.indexOf(item);
+//     this.#items.splice(itemIndex, 1);
+//   }
+// }
+
+// // Пиши код выше этой строки
+// const storage = new Storage(["Нанитоиды", "Пролонгер", "Антигравитатор"]);
+// console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор"]
+// storage.addItem("Дроид");
+// console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор", "Дроид"]
+// storage.removeItem("Пролонгер");
+// console.log(storage.getItems()); // ["Нанитоиды", "Антигравитатор", "Дроид"]
+
+//----15-19----
+class Car {
+  // Пиши код ниже этой строки
+  static MAX_PRICE = 50000;
+  #price;
+
+  constructor({ price }) {
+    this.#price = price;
+  }
+
+  get price() {
+    return this.#price;
+  }
+
+  set price(newPrice) {
+    if (newPrice > Car.MAX_PRICE) {
+      this.#price = newPrice;
+    }
+  }
+  // Пиши код выше этой строки
+}
+
+const audi = new Car({ price: 35000 });
+console.log(audi.price); // 35000
+
+audi.price = 49000;
+console.log(audi.price); // 49000
+
+audi.price = 51000;
+console.log(audi.price); // 49000
